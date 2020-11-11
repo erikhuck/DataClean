cc9177ce725b8c6ddae00ec4b4bcbcfaae1d0ab8
Erik Huckvale <erikhuck@gmail.com>
2020-11-11 10:09:42 -0700

    Removed some more obselete scripts

--- jobs/lin-autoencoder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-source ../env/bin/activate
-python3 main.py lin-autoencoder $1 $2 $3 $4
--- jobs/run-lin-autoencoder-jobs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {0..30}
-do
-    bash jobs/lin-autoencoder-job.sh $i
-done
