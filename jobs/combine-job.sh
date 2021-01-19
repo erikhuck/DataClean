f0c4f968960aafe6656ca990d58ff501c7559c17
Erik Huckvale <erikhuck@gmail.com>
2021-01-18 17:40:34 -0700

    submodule update; Update combine job submission file

--- jobs/combine-job.sh
+++ jobs/combine-job.sh
@@ -9,7 +9,7 @@ sbatch -J $JOB_NAME \
     --time=03-00:00:00 \
     --nodes=1 \
     --ntasks=1 \
-    --mem=1024G \
+    --mem=1600G \
     -o ${JOB_NAME}.out \
     -e ${JOB_NAME}.err \
     jobs/${SCRIPT_NAME}.sh ${COHORT} ${DATASET}
